c23023d80dd9501b8798a556c738e3b10e328adf
BigDreamDev <asmakech115@gmail.com>
2023-02-19 19:17:40 -0800

    Remove node_modules

--- node_modules/@syncfusion/ej2-popups/dist/global/index.d.ts
+++
@@ -1,5 +0,0 @@
-import * as _popups from '@syncfusion/ej2-popups';
-
-export declare namespace ej {
-    const popups: typeof _popups;
-}
--- node_modules/@syncfusion/ej2-richtexteditor/dist/global/index.d.ts
+++
@@ -1,5 +0,0 @@
-import * as _richtexteditor from '@syncfusion/ej2-richtexteditor';
-
-export declare namespace ej {
-    const richtexteditor: typeof _richtexteditor;
-}
