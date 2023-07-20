db5eaee08704673e684d16ae7353d1ac0d84d3cb
BigDreamDev <asmakech115@gmail.com>
2023-07-20 10:36:29 -0700

    Save Dashboard

---
+++ src/views/Dashboard1.vue
@@ -0,0 +1,71 @@
+
+<template>
+  <div class="page-wraper">
+        <div class="section-full content-inner wedding-venues-details bg-gray">
+            <div class="container">
+				<div class="row ">
+					<!-- Left part start -->
+					<div class="col-xl-12 col-lg-12 col-md-12 p-b30">
+						<!-- Reviews -->
+						<div class="venues-reviews-box" v-for="item in eventList">
+							<div class="venues-reviews">
+								<div class="reviews-info">
+									<div class="reviews-user">
+										<h6 class="reviewer-name">{{ item.title }}</h6>
+										<h6 class="wedding-date" style="text-align: left;">{{ item.createdOn }}</h6>
+									</div>	
+									<div class="reviews-rating">
+										<ul class="rating">
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star"></i></li>
+										</ul>
+										<h6 class="review-date">{{ item.createdOn }}</h6>
+									</div>
+								</div>
+								<div class="reviews-content">
+									<div class="review-media">
+										<img src="images/review/thum3.jpg" alt=""/>
+									</div>
+									<h5 class="title" style="text-align: left;">{{ item.shortDescription }}</h5>
+									<p style="text-align: left;">The best photographer any bride ( especially one who does not like her photo being taken!) could ask for! Can not thank you enough Chanon.</p>
+								</div>
+							</div>
+						</div>
+						<!-- Reviews End -->
+					</div>
+          		</div>
+        	</div>
+      </div>
+  </div>
+</template>
+
+<script>
+	import Header from "@/views/layouts/Header.vue";
+	import axios from 'axios';
+
+	export default {
+		name: "setting",
+		components: {
+			Header,
+		},
+		data() {
+			return {
+			eventList: [],
+			port: null
+			}
+		},
+		mounted() {
+			this.port = location.port;
+			axios
+			.get("http://localhost:" + this.port + "/events/subEventsForAccount")
+			.then((res) => {
+				this.eventList = res.data;
+			})
+			.catch((err) => {
+			console.log(err)
+			// window.toastr.error("Add Failed!");
+			});
+		},
+	}
+</script>
--- src/views/subEventList.vue
+++ src/views/subEventList.vue
@@ -43,71 +43,8 @@
               </div>
             </div>
           </div>
-          <!-- Search Filter -->
-          <div class="search-filter">
-            <div class="container">
-              <form class="filter-form" action="wedding-venues-search.html">
-                <div class="row">
-                  <div class="col-lg-4 col-md-4 col-sm-6 col-6">
-                    <input
-                      type="text"
-                      class="form-control"
-                      placeholder="We’re looking for"
-                    />
-                  </div>
-                  <div class="col-lg-3 col-md-3 col-sm-6 col-6">
-                    <input
-                      type="text"
-                      class="form-control"
-                      placeholder="Near"
-                    />
-                  </div>
-                  <div class="col-lg-3 col-md-3 col-sm-6 col-6">
-                    <input
-                      type="text"
-                      class="form-control"
-                      placeholder="Or Called"
-                    />
-                  </div>
-                  <div class="col-lg-2 col-md-2 col-sm-6 col-6 d-flex">
-                    <button class="btn btn-block gradient text-uppercase">
-                      Search
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-          <!-- Search Filter END -->
+
         </div>
-        <!-- Section Banner END -->
-        <!-- Manager Tools -->
-        <!-- <div class="section-full bg-white plan-tools-bx">
-          <div class="container">
-            <ul class="plan-tools-list">
-              <li
-                v-for="subevent in subeventData"
-                data-wow-duration="2s"
-                data-wow-delay="0.1s"
-                @click="openDetails(subevent.eventId)"
-              >
-                <div
-                  :class="
-                    selected == subevent.eventId
-                      ? 'list-box home-list active'
-                      : 'list-box home-list'
-                  "
-                >
-                  <h6 class="title">
-                    <i class="la la-home"></i>{{ subevent.title }}
-                  </h6>
-                  <p>150 days to go</p>
-                </div>
-              </li>
-              
-            </ul>
-          </div>
-        </div> -->
 
         <TopSubEvent :mainEventId="this.mainEventId" :defaultSelect="this.eventId" @click-subevent="openDetails"></TopSubEvent>
 
